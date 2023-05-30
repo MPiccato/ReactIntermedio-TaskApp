963d1616a20fa182a317a5244eb577289f5e43dd
mpiccato <mpiccato@me.com>
2023-05-29 21:28:48 -0300

    mejor estructurado de la app

---
+++ src/pages/404/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>No se encuentra la página</h1>
+        </div>
+    );
+}
+
+export default NotFoundPage;
